5cf050cf8df7e8d1c1940137fa7e777b870e6176
AndriyBabushka <andriy.babushka@gmail.com>
2021-07-21 18:37:44 +0300

    Branch_commit

--- ProjectGIT/ProjectGIT.cpp
+++ ProjectGIT/ProjectGIT.cpp
@@ -3,6 +3,6 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "Commit_1\n";
-    std::cout << "Commit_2\n";
+    std::cout << "Commit_1_Branch\n";
+    std::cout << "Commit_2_Branch\n";
 }

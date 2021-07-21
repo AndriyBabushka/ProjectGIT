8d8cf65dc081e1d54ec0ecc53e8e039d1d252071
AndriyBabushka <andriy.babushka@gmail.com>
2021-07-21 19:41:34 +0300

    CloneCommmite

--- ProjectGIT/ProjectGIT.cpp
+++ ProjectGIT/ProjectGIT.cpp
@@ -7,6 +7,6 @@ int main()
     std::cout << "Commit_2_Branch\n";
     std::cout << "Master_1\n";
     std::cout << "Master_2\n";
-    std::cout << "Current\n";
-    std::cout << "Current\n";
+    std::cout << "Current_Clone\n";
+    std::cout << "Current_Clone\n";
 }
